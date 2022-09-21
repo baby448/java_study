2159bff25c0e386a31742bb943b98ba250efcb1f
yh <3065131058@qq.com>
2022-09-21 16:32:11 +0800

    新增b属性，并将所有属性添加get，set方法

--- src/TestGit/Test01.java
+++ src/TestGit/Test01.java
@@ -4,5 +4,37 @@ public class Test01 {
     private int age;
     private  String name;
     private  String a;
+    private String b;
 
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getA() {
+        return a;
+    }
+
+    public void setA(String a) {
+        this.a = a;
+    }
+
+    public String getB() {
+        return b;
+    }
+
+    public void setB(String b) {
+        this.b = b;
+    }
 }
